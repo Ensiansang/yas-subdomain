ba0661c51c96e89f12102b1d5fb917eecb4ab06c
Naive-Thod <61328509+Ensiansang@users.noreply.github.com>
2023-08-09 18:10:27 +0630

    Update sidebar.blade.php

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -40,13 +40,9 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <!-- <li><a href="{{ route('user.view') }}"><i class="ti-more"></i>View User</a></li>
-            <li><a href="{{ route('user.add') }}"><i class="ti-more"></i>Add User</a></li> -->
             <li class="{{ ($route == 'user.view')?'active':'' }}"><a href="{{ route('user.view') }}"><i class="ti-more"></i>View User</a></li>
             <li class="{{ ($route == 'user.add')?'active':'' }}"><a href="{{ route('user.add') }}"><i class="ti-more"></i>Add User</a></li>
             <li class="{{ ($route == 'user.edit')?'active':'' }}"></li>
-<!-- <li class="{{ request()->routeIs('user.view') ? 'active' : '' }}"><a href="{{ route('user.view') }}"><i class="ti-more"></i>View User</a></li>
-<li class="{{ request()->routeIs('user.add') ? 'active' : '' }}"><a href="{{ route('user.add') }}"><i class="ti-more"></i>Add User</a></li> -->
           </ul>
         </li> 
 
@@ -76,7 +72,6 @@
             <li class="{{ ($route == 'profile.view')?'active':'' }}"><a href="{{ route('profile.view') }}"><i class="ti-more"></i>Your Profile</a></li>
             <li class="{{ ($route == 'password.view')?'active':'' }}"><a href="{{ route('password.view') }}"><i class="ti-more"></i>Change Password</a></li>
             <li class="{{ ($route == 'profile.edit')?'active':'' }}"> </li>
-            <!-- <li><a href="mailbox_read_mail.html"><i class="ti-more"></i>Read</a></li> -->
           </ul>
         </li>
 
@@ -121,40 +116,12 @@
             <li class="{{ ($route == 'assign.subject.edit')?'active':'' }}"> </li> 
           </ul>
         </li>
-
-        
-       
-		 
-        <!-- <li class="header nav-small-cap">User Interface</li>
-		  
-        <li class="treeview">
-          <a href="#">
-            <i data-feather="grid"></i>
-            <span>Components</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="components_alerts.html"><i class="ti-more"></i>Alerts</a></li>
-            <li><a href="components_badges.html"><i class="ti-more"></i>Badge</a></li>
-            
-          </ul>
-        </li> -->
 		
 
         
       </ul>
     </section>
 	
-	<!-- <div class="sidebar-footer">
-		item
-		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
-		item
-		<a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ti-email"></i></a>
-		item
-		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ti-lock"></i></a>
-	</div> -->
   </aside>
 <script>
   document.getElementById('manageUserMenuItem').addEventListener('click', function() {
