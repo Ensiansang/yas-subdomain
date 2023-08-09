22bcd45518d76127064590379caf406e1cf7191f
Naive-Thod <61328509+Ensiansang@users.noreply.github.com>
2023-08-09 18:11:45 +0630

    Update student_add.blade.php

--- resources/views/admin/student_reg/student_add.blade.php
+++ resources/views/admin/student_reg/student_add.blade.php
@@ -55,27 +55,7 @@
  			
  			<div class="col-md-4">
 
- 		 <!-- <div class="form-group">
-		<h5>Student Name <span class="text-danger">*</span></h5>
-		<div class="controls">
-        <select name="student"  required="" class="form-control">
-  <option value="" selected="" disabled="">Select Student</option>
-   @foreach($students as $student)
-  <option value="{{ $student->id }}">{{ $student->name }}</option>
-   @endforeach
-   
-</select>
-	  </div>		 
-	  </div> -->
-
-      <!-- <div class="form-group">
-  <h5>Student Name <span class="text-danger">*</span></h5>
-  <div class="controls">
-    <input type="text" name="student" required="" class="form-control" id="student_name">
-    <select id="student_dropdown" class="form-control" style="margin-top: 5px;"></select>
-  </div>		 
-</div> -->
-
+ 		
 <div class="form-group">
   <h5>Student Name <span class="text-danger">*</span></h5>
   <div class="controls">
@@ -88,18 +68,6 @@
     </select>
   </div>		 
 </div>
-
-
-
-
-<!-- 
-      <div class="form-group">
-  <h5>Student Name<span class="text-danger">*</span></h5>
-  <div class="controls">
-    <input name="search" name="student_name" id="student_name" class="form-control" required>
-  </div>		 
-</div> -->
-
 
  			</div> <!-- End Col md 4 -->
 
@@ -167,33 +135,6 @@
 
 <div class="row"> <!-- 5TH Row -->
 
-
-
-
-
-
- 			
- 		<!-- <div class="col-md-4">
-
- 		<div class="form-group">
-		<h5>Profile Image <span class="text-danger">*</span></h5>
-		<div class="controls">
-	 <input type="file" name="image" class="form-control" id="image" >  </div>
-	 </div>
-	  
- 			</div> End Col md 4  -->
-
-
-<!-- <div class="col-md-4">
-
- 		<div class="form-group">
-		<div class="controls">
-	<img id="showImage" src="{{ url('upload/no_image.jpg') }}" style="width: 100px; width: 100px; border: 1px solid #000000;"> 
-
-	 </div>
-	 </div>
-	  
- 			</div> End Col md 4  -->
  
  			
  		</div> <!-- End 5TH Row -->
@@ -224,51 +165,12 @@
 	  </div>
   </div>
 
-<!-- <script>
-    $(document).ready(function() {
-  $('#student_name').on('keyup', function() {
-    var searchQuery = $(this).val();
 
-    $.ajax({
-      url: '/search-students', // Replace with the actual route URL
-      method: 'GET',
-      data: { name: searchQuery },
-      success: function(response) {
-        var students = response.students;
-
-        // Clear previous results
-        $('#student_results').empty();
-
-        // Populate the results in a dropdown or list
-        students.forEach(function(student) {
-          var option = $('<option>').val(student.id).text(student.name);
-          $('#student_results').append(option);
-        });
-      }
-    });
-  });
-});
-
-</script> -->
 
 <script>
     $(document).ready(function() {
   $('#student_name').on('input', function() {
     var searchQuery = $(this).val();
-
-// $(document).ready(function() {
-//   $('#student_dropdown').on('change', function() {
-//     var selectedStudentId = $(this).val();
-//     $('#student_name').val(selectedStudentId);
-//   });
-// });
-// $(document).ready(function() {
-//   $('#student_dropdown').on('change', function() {
-//     var selectedStudentId = $(this).val();
-//     $('input[name="student"]').val(selectedStudentId);
-//   });
-// });
-
 
     $.ajax({
       url: '/search-students', // Replace with the actual route URL
