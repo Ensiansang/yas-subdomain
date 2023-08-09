c0e14d0c06e51389a02e8c0d189e7b731bfc6bcc
Naive-Thod <61328509+Ensiansang@users.noreply.github.com>
2023-08-09 17:59:08 +0630

    Update StudentController.php

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -22,58 +22,16 @@ class StudentController extends Controller
         $userName = Auth::user()->name;
         $userData = User::find($id);
          // Get the currently authenticated student's ID
-    $studentId = Auth::id();
+        $studentId = Auth::id();
 
     // Fetch the grade records for the student
-    // $grades = Grade::where('user_id', $studentId)->get();
-    $grades = Grade::with('assign_subject.school_subject')
+       $grades = Grade::with('assign_subject.school_subject')
         ->where('user_id', $studentId)
         ->get();
 
 
         return view('student.index',compact('userData', 'grades','userName'));
     }
-
-    // public function Download($id, $type)
-    // {
-    //     // $report = GradeReportCard::findOrFail($id);
-    //     // Get the currently authenticated student's ID
-    // $studentId = Auth::id();
-    
-    // // Fetch the GradeReportCard based on the $id and the user_id
-    // $report = GradeReportCard::where('id', $id)
-    //     ->where('user_id', $studentId)
-    //     ->firstOrFail();
-    //     if ($type === 'pdf') {
-    //     // Check if the PDF file exists
-    //     if ($report->pdf_file_path && Storage::exists('upload/grade_reports_pdf/' . $report->pdf_file_path)) {
-    //         $pdfFilePath = public_path('upload/grade_reports_pdf' . $report->pdf_file_path); // Use public_path() to get the correct public directory path
-    //        // $pdfFileName = 'custom_pdf_name.pdf';
-    //         // return response()->download($pdfFilePath, $pdfFileName);
-    //        // Set content disposition header for PDF download
-    //        $headers = [
-    //         'Content-Type' => 'application/pdf',
-    //         'Content-Disposition' => 'attachment; filename="' . basename($pdfFilePath) . '"',
-    //     ];
-    //         return response()->download($pdfFilePath, basename($pdfFilePath));
-    //     }
-    
-    //    }elseif ($type === 'image') {
-    //       // Check if the image file exists
-    //     if ($report->image_file_path && Storage::exists('upload/grade_reports_img/' . $report->image_file_path)) {
-    //         $imageFilePath = public_path('upload/grade_reports_img' . $report->image_file_path); // Use public_path() to get the correct public directory path
-    //         // $imageFileName = 'custom_image_name.jpg'; // Provide a custom name for image file
-    //         // return response()->download($imageFilePath, $imageFileName);
-    //         $headers = [
-    //             'Content-Type' => 'image/jpeg', // Change this to the appropriate image content type if needed
-    //             'Content-Disposition' => 'attachment; filename="' . basename($imageFilePath) . '"',
-    //         ];
-    //         return response()->download($imageFilePath, basename($imageFilePath));
-    //     }
-    //    }
-    //     // If both file paths are empty or the files do not exist, redirect back with an error message
-    //     return redirect()->back()->with('error', 'File not found.');
-    // }
 
     public function Download($id, $type)
 {
@@ -89,7 +47,6 @@ class StudentController extends Controller
     if ($type === 'pdf') {
         // Check if the PDF file exists
         $pdfFilePath = public_path('upload/grade_reports_pdf'. DIRECTORY_SEPARATOR . $report->pdf_file_path);
-        // if (Storage::exists('upload/grade_reports_pdf' . $report->pdf_file_path)) {
             $headers = [
                 'Content-Type' => 'application/pdf',
                 'Content-Disposition' => 'attachment; filename="' . basename($pdfFilePath) . '"',
@@ -103,7 +60,6 @@ class StudentController extends Controller
     elseif ($type === 'image') {
         // Check if the image file exists
         $imageFilePath = public_path('upload/grade_reports_img'. DIRECTORY_SEPARATOR . $report->image_file_path);
-        // if (Storage::exists('upload/grade_reports_img' . $report->image_file_path)) {
             $headers = [
                 'Content-Type' => 'image/jpeg', // Change this to the appropriate image content type if needed
                 'Content-Disposition' => 'attachment; filename="' . basename($imageFilePath) . '"',
